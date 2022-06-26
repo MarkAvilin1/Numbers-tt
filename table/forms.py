5e8266345539f76566d791021173cc872c25d6fd
markavilin1 <avilin1@outlook.com>
2022-06-26 13:15:25 -0300

    Add new Javascript function

--- table/forms.py
+++ table/forms.py
@@ -1,8 +1,24 @@
 from django.forms import ModelForm
 from .models import Order
+from django import forms
 
 
-class OrderForm(ModelForm):
-    class Meta:
-        model = Order
-        fields = "__all__"
+class OrderForm(forms.Form):
+    order_num = forms.CharField(max_length=50,
+                                widget=forms.TextInput
+                                (attrs={'id': 'order'}))
+    price_usd = forms.CharField(max_length=50,
+                                widget=forms.TextInput
+                                (attrs={'id': 'usd',
+                                        'onkeyup': 'usd2rub()',
+                                        }))
+    price_rub = forms.CharField(max_length=50,
+                                widget=forms.TextInput
+                                (attrs={'id': 'rub'}))
+    delivery_time = forms.CharField(max_length=50,
+                                    widget=forms.TextInput
+                                    (attrs={'id': 'dt'}))
+
+    # class Meta:
+    #     model = Order
+    #     fields = "__all__"
