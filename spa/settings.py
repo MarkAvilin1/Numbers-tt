220dbee782b5cb74040bc8bcdcd247bf2cc701e9
markavilin1 <avilin1@outlook.com>
2022-10-04 22:50:39 -0300

    New update

--- spa/settings.py
+++ spa/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*027i0rhjg-1=b&wjgo@krk#8qsjdxm2o&bo_lmd302cfa#q5d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
